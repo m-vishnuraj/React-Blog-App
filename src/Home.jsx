94f16d593e5376b58cbf3a0fcff36736f5f004f3
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-26 20:26:14 +0530

    outputting list completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'vishnu', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     return (
-        <div>Home</div>
+        <div className="home">
+            {blogs.map(blog => (
+                <div className="blog_preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
